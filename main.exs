95a5bd46fd3034ccb3fb0f4668c8067e2a687ce3
Alex Neyman <leshaneyman@gmail.com>
2022-12-09 09:40:36 +0200

    Automate imports

--- main.exs
+++ main.exs
@@ -1,32 +1,12 @@
-Enum.map(Path.wildcard("./day*"), fn day ->
-  Code.require_file("#{day}.exs", "./#{day}")
-end)
-
 defmodule CodeAdvent2022 do
-  alias CodeAdvent2022.{Day1, Day2}
-
-  @spec run([String.t()]) :: String.t()
   def run(args) do
     {day, part, input_name} = parse_args(args)
 
     input = File.read!(Path.relative_to_cwd("./day#{day}/input_#{input_name}.txt"))
 
-    case {day, part} do
-      {1, 1} ->
-        Day1.solve1(input)
+    {module, function} = find_solver_module(day, part)
 
-      {1, 2} ->
-        Day1.solve2(input)
-
-      {2, 1} ->
-        Day2.solve1(input)
-
-      {2, 2} ->
-        Day2.solve2(input)
-
-      _ ->
-        raise "unexpected day #{day}"
-    end
+    apply(module, function, [input])
   end
 
   defp parse_args(args) do
@@ -35,8 +15,21 @@ defmodule CodeAdvent2022 do
         {String.to_integer(day), String.to_integer(part), input_name}
 
       _ ->
-        raise "unexpected number of arguments"
+        raise "Unexpected number of arguments"
     end
+  end
+
+  defp find_solver_module(day, part) do
+    Code.require_file("day#{day}.exs", "./day#{day}")
+
+    module = String.to_existing_atom("Elixir.CodeAdvent2022.Day#{day}")
+    function = String.to_existing_atom("solve#{part}")
+
+    unless function_exported?(module, function, 1) do
+      raise "Function #{module}.#{function}/1 is not exported"
+    end
+
+    {module, function}
   end
 end
 
